a248404c6140b4cbb540be375385f4b8297633ca
swon95 <shimtkddnjs13@gmail.com>
2023-07-29 16:39:07 +0900

    feat: form container

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,59 @@
 import useMetamask from "./hooks/useMetamask";
 import { formatChainAsNum } from "./utils/formmatter";
+import FormContainer from "./components/FormContainer";
 
 function App() {
-  const {
-    hasProvider,
-    wallet,
-    disableConnect,
-    handleConnect,
-    error,
-    errorMessage,
-    setError,
-  } = useMetamask();
+    const {
+        hasProvider,
+        wallet,
+        disableConnect,
+        handleConnect,
+        error,
+        errorMessage,
+        setError,
+    } = useMetamask();
 
-  return (
-    <div className="w-full flex flex-col justify-center items-center p-8 gap-4">
-      <h1 className="font-bold text-4xl">UNDEFINED TOKYO</h1>
+    return (
+        <div className="w-full flex flex-col justify-center items-center p-8 gap-4">
+            <h1 className="font-bold text-4xl">UNDEFINED TOKYO</h1>
 
-      {!hasProvider && <p>Please Install Metamask.</p>}
+            {!hasProvider && <p>Please Install Metamask.</p>}
 
-      {window.ethereum?.isMetaMask && wallet.accounts.length < 1 && (
-        <button
-          className="flex items-center gap-2 border border-gray-600 rounded p-2 text-gray-700 hover:bg-gray-100"
-          onClick={handleConnect}
-          disabled={disableConnect}
-        >
-          <img
-            className="w-8 h-8"
-            src={
-              "https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg"
-            }
-          />
-          <p className="text-lg">Connect MetaMask</p>
-        </button>
-      )}
+            {window.ethereum?.isMetaMask && wallet.accounts.length < 1 && (
+                <button
+                    className="flex items-center gap-2 border border-gray-600 rounded p-2 text-gray-700 hover:bg-gray-100"
+                    onClick={handleConnect}
+                    disabled={disableConnect}
+                >
+                    <img
+                        className="w-8 h-8"
+                        src={
+                            "https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg"
+                        }
+                    />
+                    <p className="text-lg">Connect MetaMask</p>
+                </button>
+            )}
 
-      {wallet.accounts.length > 0 && (
-        <>
-          <div>Wallet Accounts: {wallet.accounts[0]}</div>
-          <div>Wallet Balance: {wallet.balance}</div>
-          <div>Hex ChainId: {wallet.chainId}</div>
-          <div>Numeric ChainId: {formatChainAsNum(wallet.chainId)}</div>
-        </>
-      )}
+            {wallet.accounts.length > 0 && (
+                <>
+                    <div>Wallet Accounts: {wallet.accounts[0]}</div>
+                    <div>Wallet Balance: {wallet.balance}</div>
+                    <div>Hex ChainId: {wallet.chainId}</div>
+                    <div>
+                        Numeric ChainId: {formatChainAsNum(wallet.chainId)}
+                    </div>
+                    <FormContainer />
+                </>
+            )}
 
-      {error && (
-        <div onClick={() => setError(false)}>
-          <strong>Error:</strong> {errorMessage}
+            {error && (
+                <div onClick={() => setError(false)}>
+                    <strong>Error:</strong> {errorMessage}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default App;
