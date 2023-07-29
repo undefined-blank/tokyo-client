6650a516f77c0afd3e973ea0938f5115c56471ca
yunsu <than4502@gmail.com>
2023-07-29 14:45:47 +0900

    chore: title, metamas icon

--- src/App.tsx
+++ src/App.tsx
@@ -14,26 +14,32 @@ function App() {
 
   return (
     <div className="w-full flex flex-col justify-center items-center p-8 gap-4">
-      <h1 className="font-bold text-2xl">UNDEFINED TOKYO</h1>
+      <h1 className="font-bold text-4xl">UNDEFINED TOKYO</h1>
 
       {!hasProvider && <p>Please Install Metamask.</p>}
 
       {window.ethereum?.isMetaMask && wallet.accounts.length < 1 && (
         <button
-          className="border border-gray-600 rounded p-2 text-gray-700 hover:bg-gray-100"
+          className="flex items-center gap-2 border border-gray-600 rounded p-2 text-gray-700 hover:bg-gray-100"
           onClick={handleConnect}
           disabled={disableConnect}
         >
-          Connect MetaMask
+          <img
+            className="w-8 h-8"
+            src={
+              "https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg"
+            }
+          />
+          <p className="text-lg">Connect MetaMask</p>
         </button>
       )}
 
       {wallet.accounts.length > 0 && (
         <>
           <div>Wallet Accounts: {wallet.accounts[0]}</div>
-          <div>Wallet Balance: {wallet.balance}</div> {}
-          <div>Hex ChainId: {wallet.chainId}</div> {}
-          <div>Numeric ChainId: {formatChainAsNum(wallet.chainId)}</div>{" "}
+          <div>Wallet Balance: {wallet.balance}</div>
+          <div>Hex ChainId: {wallet.chainId}</div>
+          <div>Numeric ChainId: {formatChainAsNum(wallet.chainId)}</div>
         </>
       )}
 
