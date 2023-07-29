06319da7119347159be6d13f397e16137db2d27b
yunsu <than4502@gmail.com>
2023-07-29 13:21:13 +0900

    feat: vite + tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full flex flex-col justify-center items-center p-8 gap4">
+      <h1 className="font-bold text-2xl">UNDEFINED TOKYO</h1>
+    </div>
+  );
+}
+
+export default App;
